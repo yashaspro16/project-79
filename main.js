536a98d259a4296b2e375889a206178cf589dbe6
yashaspro16 <89565619+yashaspro16@users.noreply.github.com>
2021-11-10 15:24:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+menu_list_array = ["Veg Margherita Pizza", "Chicken Pizza", "Paneer Tikka Pizza", "Barbeque Pizza", "Veg Pizza", "Chicken Tikka Pizza"//add more items
+                    ];
+
+function getmenu(){
+var htmldata="";
+menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ menu_list_array[i] + '<br>'
+        }
+        document.getElementById("display_menu").innerHTML = htmldata;
+        //give the appropriate id name as display_menu  
+}
+
+function add_item(){
+var htmldata;
+var imgtags='<img id="im1" src="pizza.png">';
+var item=document.getElementById("add_item").value;
+menu_list_array.sort();
+        // use the sort function as - menu_list_array.sort();
+        htmldata=""
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+        }
+         document.getElementById("display_addedmenu").innerHTML = htmldata;		
+      }
+
+function add_top(){
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	add_item();
+}
